cb065bda83dabae9a950077520e3b3820637aab0
koray öngün <koraygg28@gmail.com>
2023-03-11 17:43:46 +0300

    11032023

---
+++ NumberReading/Program.cs
@@ -0,0 +1,25 @@
+﻿using NumberReading;
+
+Okuma okuma = new Okuma();
+string result = string.Empty;
+
+
+int number = 11;
+// number = 1123076
+if (number == 0)
+{
+    Console.WriteLine("sıfır");
+}
+else
+{
+    string sNumber = number.ToString();
+    //sNumber = "1123076"
+    okuma.Bol(ref sNumber);
+
+    string[] numbers = sNumber.Split(',');
+    // numbers = {"1","123","076"}
+
+
+    result = okuma.Oku(numbers);
+    Console.WriteLine(result);
+}
